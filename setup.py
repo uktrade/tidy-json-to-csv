315f0f9724a2fc188971215bfe438fd3d8994509
Michal Charemza <michal@charemza.name>
2020-06-07 19:44:47 +0100

    (release) v0.0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setuptools.setup(
     name='tidy-json-to-csv',
-    version='0.0.2',
+    version='0.0.3',
     author='Department for International Trade',
     author_email='webops@digital.trade.gov.uk',
     description='Convert JSON to a set of tidy CSV files',
