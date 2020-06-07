26d496375fa2a533bd431e4f1943e61003549d88
Michal Charemza <michal@charemza.name>
2020-06-07 18:21:48 +0100

    (feat) Initial behaviour

---
+++ tests.py
@@ -0,0 +1,65 @@
+import unittest
+
+from tidy_json_to_csv import to_csvs
+
+
+class TestIntegration(unittest.TestCase):
+
+    def test_basic(self):
+        total_received = {}
+
+        def json_bytes():
+            yield json_bytes_songs
+
+        def save_csv(path):
+            total_received[path] = []
+            while True:
+                bytes_chunked = yield
+                total_received[path].append(bytes_chunked)
+
+        to_csvs(json_bytes(), save_csv)
+        files = {
+            path: b''.join(contents)
+            for path, contents in total_received.items()
+        }
+        self.assertEqual(set(files.keys()), set([
+            'songs[*]', 'songs[*].categories[*]', 'songs[*].categories[*].id',
+            'songs[*].comments[*]',
+        ]))
+        self.assertEqual(files, json_bytes_songs_parsed)
+
+
+json_bytes_songs = b'''{
+  "songs": [
+    {
+      "id": "1",
+      "title": "Walk through the fire",
+      "categories": [
+        {"id": "1", "name": "musicals"},
+        {"id": "2", "name": "television-shows"}
+      ],
+      "comments": [
+        {"content": "I love it"},
+        {"content": "I've heard better"}
+      ]
+    },
+    {
+      "id": "2",
+      "title": "I could have danced all night",
+      "categories": [
+        {"id": "1", "name": "musicals"},
+        {"id": "3", "name": "films"}
+      ],
+      "comments": [
+        {"content": "I also could have danced all night"}
+      ]
+    }
+  ]
+}'''
+
+json_bytes_songs_parsed = {
+    'songs[*].categories[*].id': b'\xef\xbb\xbf"songs.id","categories.id"\r\n"1","1"\r\n"1","2"\r\n"2","1"\r\n"2","3"\r\n',
+    'songs[*].categories[*]': b'\xef\xbb\xbf"id","name"\r\n"1","musicals"\r\n"2","television-shows"\r\n"3","films"\r\n',
+    'songs[*].comments[*]': b'\xef\xbb\xbf"songs.id","content"\r\n"1","I love it"\r\n"1","I\'ve heard better"\r\n"2","I also could have danced all night"\r\n',
+    'songs[*]': b'\xef\xbb\xbf"id","title"\r\n"1","Walk through the fire"\r\n"2","I could have danced all night"\r\n',
+}
---
+++ tidy_json_to_csv.py
@@ -0,0 +1,130 @@
+from collections import defaultdict
+import codecs
+import csv
+import re
+import ijson
+
+
+def to_csvs(json_bytes, save_csv_bytes, null='#NA'):
+    top_level_maps = {}
+    top_level_saved = defaultdict(set)
+    open_maps = {}
+    parent_ids = []
+    open_csv_gens = {}
+
+    class PseudoBuffer:
+        def write(self, value):
+            return value
+
+    csv_writer = csv.writer(PseudoBuffer(), quoting=csv.QUOTE_NONNUMERIC)
+
+    def save(path, dict_data):
+        try:
+            gen = open_csv_gens[path]
+        except KeyError:
+            gen = save_csv_bytes(path)
+            open_csv_gens[path] = gen
+            next(gen)
+            gen.send(codecs.BOM_UTF8)
+            gen.send(csv_writer.writerow(dict_data.keys()).encode('utf-8'))
+
+        gen.send(csv_writer.writerow(dict_data.values()).encode('utf-8'))
+
+    def to_path(prefix):
+        components = re.findall(r'([^.]+)\.item', prefix)
+        return '.'.join([f'{component}[*]' for component in components])
+
+    def handle_start_map(prefix, value):
+        open_maps[prefix] = {}
+
+    def handle_end_map(prefix, value):
+        # parent, _, key = prefix.rpartition('.')
+        key = prefix.rpartition('.item')[0].rpartition('.')[2]
+        is_top_level = 'id' in open_maps[prefix]
+
+        # IDs of parents so the user can do JOINs
+        parent_id_dict = {
+            f'{parent_key}.id': parent_id
+            for (parent_key, parent_id) in parent_ids
+        }
+
+        # ... and only save these for nested top level
+        if is_top_level and len(parent_ids) > 1:
+            save(to_path(prefix) + '.id', parent_id_dict)
+
+        # ... but if _not_ top level (i.e. no ID), save the IDs and other data
+        if not is_top_level and len(parent_ids):
+            save(to_path(prefix), {**parent_id_dict, **open_maps[prefix]})
+
+        # ... and if top level, but not yet saved it, save it
+        if is_top_level and open_maps[prefix]['id'] not in top_level_saved[key]:
+            save(to_path(prefix), open_maps[prefix])
+            top_level_saved[key].add(open_maps[prefix]['id'])
+
+        # We're going to be moving up a level, so no need for last ID
+        if is_top_level:
+            parent_ids.pop()
+
+        del open_maps[prefix]
+
+    def handle_map_key(prefix, value):
+        pass
+
+    def handle_start_array(prefix, value):
+        pass
+
+    def handle_end_array(prefix, value):
+        pass
+
+    def handle_null(prefix, _):
+        parent, _, key = prefix.rpartition('.')
+        open_maps[parent][key] = null
+
+    def handle_boolean(prefix, value):
+        parent, _, key = prefix.rpartition('.')
+        open_maps[parent][key] = value
+
+    def handle_integer(prefix, value):
+        parent, _, key = prefix.rpartition('.')
+        open_maps[parent][key] = value
+
+    def handle_double(prefix, value):
+        parent, _, key = prefix.rpartition('.')
+        open_maps[parent][key] = value
+
+    def handle_number(prefix, value):
+        parent, _, key = prefix.rpartition('.')
+        open_maps[parent][key] = value
+
+    def handle_string(prefix, value):
+        parent, _, key = prefix.rpartition('.')
+        open_maps[parent][key] = value
+
+        if key == 'id':
+            parent_key = prefix.rpartition('.item.')[0].rpartition('.item.')[2]
+            parent_ids.append((parent_key, value))
+
+    handlers = locals()
+
+    def process(events):
+        for prefix, event, value in events:
+            handlers[f'handle_{event}'](prefix, value)
+
+    try:
+        events = ijson.sendable_list()
+        coro = ijson.parse_coro(events)
+        for chunk in json_bytes:
+            coro.send(chunk)
+            process(events)
+            del events[:]
+
+        coro.close()
+        process(events)
+
+    # Close all open CSVs
+    finally:
+        for gen in open_csv_gens:
+            try:
+                gen.close()
+            except:
+                pass
